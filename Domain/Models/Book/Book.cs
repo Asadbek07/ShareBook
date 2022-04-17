1d64061e11b418d085b4d06420973ca396bd3c4c
Asadbek <asindarov07@gmail.com>
2022-04-17 17:47:18 +0500

    Added published date

--- Domain/Models/Book/Book.cs
+++ Domain/Models/Book/Book.cs
@@ -4,6 +4,7 @@
     {
         public string Title { get; set; }
         public string ISBN { get; set; }
+        public DateTimeOffset PublishedDate { get; set; }
         public bool IsDeleted { get; set; } = false;
         public Guid AuthorId { get; set; }
     }
